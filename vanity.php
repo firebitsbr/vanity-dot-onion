db1baeb64917b3ed4f3edd7a97942b43cb6e0b0b
Matthew Heick <git@heick.email>
2018-09-08 13:34:26 -0400

    finally got a matching example between python and php, and the reason why...

--- vanity.php
+++ vanity.php
@@ -3,58 +3,87 @@
 // Source: https://gitweb.torproject.org/torspec.git/tree/rend-spec-v2.txt, 1.5
 require_once('base32_rfc4648.php');
 
-/**
- * <XmlMatchOutput>
- *  <GeneratedDate>2014-08-05T07:14:50.329955Z</GeneratedDate>
- *  <Hash>prefix64kxpwmzdz.onion</Hash>
- *  <PrivateKey>-----BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQCmYmTnwGOCpsPOqvs5mZQbIM1TTqOHK1r6zGvpk61ZaT7z2BCE
-FPvdTdkZ4tQ3/95ufjhPx7EVDjeJ/JUbT0QAW/YflzUfFJuBli0J2eUJzhhiHpC/
-1d3rb6Uhnwvv3xSnfG8m7LeI/Ao3FLtyZFgGZPwsw3BZYyJn3sD1mJIJrQIEB/ZP
-ZwKBgCTUQTR4zcz65zSOfo95l3YetVhfmApYcQQd8HTxgTqEsjr00XzW799ioIWt
-vaKMCtJlkWLz4N1EqflOH3WnXsEkNA5AVFe1FTirijuaH7e46fuaPJWhaSq1qERT
-eQT1jY2jytnsJT0VR7e2F83FKINjLeccnkkiVknsjrOPrzkXAkEA0Ky+vQdEj64e
-iP4Rxc1NreB7oKor40+w7XSA0hyLA3JQjaHcseg/bqYxPZ5J4JkCNmjavGdM1v6E
-OsVVaMWQ7QJBAMweWSWtLp6rVOvTcjZg+l5+D2NH+KbhHbNLBcSDIvHNmD9RzGM1
-Xvt+rR0FA0wUDelcdJt0R29v2t19k2IBA8ECQFMDRoOQ+GBSoDUs7PUWdcXtM7Nt
-QW350QEJ1hBJkG2SqyNJuepH4PIktjfytgcwQi9w7iFafyxcAAEYgj4HZw8CQAUI
-3xXEA2yZf9/wYax6/Gm67cpKc3sgKVczFxsHhzEml6hi5u0FG7aNs7jQTRMW0aVF
-P8Ecx3l7iZ6TeakqGhcCQGdhCaEb7bybAmwQ520omqfHWSte2Wyh+sWZXNy49EBg
-d1mBig/w54sOBCUHjfkO9gyiANP/uBbR6k/bnmF4dMc=
+$private_key = <<<EOK
+-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQCuvF9M1eCjLAHQzEoY3AjRDES4MtMbZnoVWnLSMQKS1TUu1czm
+2CPWX4zHKVfQlEblZ9CZ5rlJDeVksz69rga4a6/zzi3xGok0kOOA6y4XGsO6spjA
+nTTaWaFRljwqt1/g2Je9HmEMPtBngxMfi93LCGD28OIn9fmlQmIDkqbUtQIEDUNw
+GQKBgAIKQG2xC78aE0j1pHVbdZgX3VrXGYfzDlVwmzcDuDAy19AWw0ATmdnaHhhS
+UsogG2ZmaeOhwftG5avcjw6mRMJvF9Ck/m1wqOYRm7fYGnGVEQMW7XKZ3URcett2
+c484r5yjkHFH3INE14+lHmZgy47m59WvrVPexwBmIfWiFcfJAkEA3JCT7y2pE0+G
+4pBwziTojpK03y+VNVtVVqI4D8Hff9lzwgZ2fjc41+qV8SHdDz+hmy5fMvGGTY8w
+kjLbX+1VDQJBAMrO71z1RBVCoB+diNw3n/rJfXKPdmnRBH+sFhAFWETaAe9hvVEk
+BlTAJRhE8mk23htOLT/rrY1csCbwuJ6i5EkCQGklBacsQCmcjgNcRzTK5R/RvWuK
+5BVNhOU/JoDA1O+f7+h0f+uXTjdFjCM3RZnk66Do3s49UFwbRXpHqRwSAC0CQQC9
+Wbl0ZJn+z8L/K1uUcf2L9hMCRyvrS7NkTPWJR+yRs56eTPPsfO+JpFfllSU1HrXJ
+3TAkpK/Cz9lqX35ShWhpAkAAiOrM9cM1gl90T1rcP8lSn/aKzaaL6zsBCJPsUKC+
+yeYM3pI7Re4yLrOdMyfC4tl6eMLkFqewP0Zk5BVg75lJ
 -----END RSA PRIVATE KEY-----
- *  </PrivateKey>
- *  <PublicModulusBytes>pmJk58BjgqbDzqr7OZmUGyDNU06jhyta+sxr6ZOtWWk+89gQhBT73U3ZGeLUN//ebn44T8exFQ43ifyVG09EAFv2H5c1HxSbgZYtCdnlCc4YYh6Qv9Xd62+lIZ8L798Up3xvJuy3iPwKNxS7cmRYBmT8LMNwWWMiZ97A9ZiSCa0=</PublicModulusBytes>
- *  <PublicExponentBytes>B/ZPZw==</PublicExponentBytes>
- * </XmlMatchOutput>
- */
+EOK;
 
-/**
- * Public Key
- * After writing above <PrivateKey> to priv.key
- * Execute: openssl rsa -pubout < priv.key > priv.pub
- */
-$public_key = 'MIGgMA0GCSqGSIb3DQEBAQUAA4GOADCBigKBgQCmYmTnwGOCpsPOqvs5mZQbIM1TTqOHK1r6zGvpk61ZaT7z2BCEFPvdTdkZ4tQ3/95ufjhPx7EVDjeJ/JUbT0QAW/YflzUfFJuBli0J2eUJzhhiHpC/1d3rb6Uhnwvv3xSnfG8m7LeI/Ao3FLtyZFgGZPwsw3BZYyJn3sD1mJIJrQIEB/ZPZw==';
-$key = base64_decode($public_key);
+$public_key = <<<EOK
+-----BEGIN PUBLIC KEY-----
+MIGgMA0GCSqGSIb3DQEBAQUAA4GOADCBigKBgQCuvF9M1eCjLAHQzEoY3AjRDES4
+MtMbZnoVWnLSMQKS1TUu1czm2CPWX4zHKVfQlEblZ9CZ5rlJDeVksz69rga4a6/z
+zi3xGok0kOOA6y4XGsO6spjAnTTaWaFRljwqt1/g2Je9HmEMPtBngxMfi93LCGD2
+8OIn9fmlQmIDkqbUtQIEDUNwGQ==
+-----END PUBLIC KEY-----
+EOK;
 
-// Hash Key with Sha1
-$key_hash = hash('sha1', $key, true);
-$key_hash_80 = substr($key_hash, strlen($key_hash)-10, 10); /* first 80 bits of this */
-
-// Base32 Encode the 80-bit hash
-$b32 = strtolower(Base32::encode($key_hash_80));
-echo "does [prefix64kxpwmzdz] = [$b32]?\n";
-
-echo "binary:\n";
-for ($x = 0; $x < strlen($key_hash) - 1; $x++)
-{
-	$ch = ord(substr($key_hash, $x, 1));
-	echo $x . ": (" . str_pad($ch, 3, " ", STR_PAD_LEFT) . ") " . str_pad(decbin($ch), 8, "0", STR_PAD_LEFT) . "\n";
+$matches = array();
+if (!preg_match('~^-----BEGIN ([A-Z ]+)-----\s*?([A-Za-z0-9+=/\r\n]+)\s*?-----END \1-----\s*$~D', $public_key, $matches) === 0) {
+	die('Invalid PEM format encountered.'."\n");
 }
+$public_key_decoded = base64_decode(str_replace(array("\r", "\n"), array('', ''), $matches[2]));
+// sources of substr($, 22):
+// > https://gist.github.com/DonnchaC/d6428881f451097f329e
+// > https://www.dlitz.net/software/pycrypto/api/current/Crypto.PublicKey.RSA._RSAobj-class.html#exportKey
+echo "onion-name: " . strtolower(Base32::encode(sha1(substr($public_key_decoded, 22), true))) . "\n";
+echo "\n";
 
-echo "known:\n";
-$known_hash_80 = Base32::decode(strtoupper("prefix64kxpwmzdz"));
-for ($x = 0; $x < strlen($known_hash_80) - 1; $x++)
+$key_resource = openssl_pkey_get_private($private_key);
+//$key_resource = openssl_pkey_get_public($public_key);
+if ($key_resource === false) { die('error loading private key'); }
+$key_details = openssl_pkey_get_details($key_resource);
+echo "bits: " . $key_details['bits'] . "\n";
+echo "key: " . $key_details['key'] . "\n";
+echo "type: ";
+switch ($key_details['type'])
 {
-	$ch = ord(substr($known_hash_80, $x, 1));
-	echo $x . ": (" . str_pad($ch, 3, " ", STR_PAD_LEFT) . ") " . str_pad(decbin($ch), 8, "0", STR_PAD_LEFT) . "\n";
+	case OPENSSL_KEYTYPE_RSA:
+		echo "OPENSSL_KEYTYPE_RSA\n";
+		$decode = array(
+			"modulus" => "n",
+			"public exponent" => "e",
+			"private exponent" => "d",
+			"prime 1" => "p",
+			"prime 2" => "q",
+			"exponent1, d mod (p-1)" => "dmp1",
+			"exponent2, d mod (q-1)" => "dmq1",
+			"coefficient, (inverse of q) mod p" => "iqmp",
+		);
+		foreach ($decode as $english => $key)
+		{
+			if (array_key_exists($key, $key_details['rsa']))
+			{
+				echo "  " . $english . ":\n";
+				echo "    " . bin2hex($key_details['rsa'][$key]) . "\n";
+				echo "    " . base64_encode($key_details['rsa'][$key]) . "\n";
+				echo "    " . sha1($key_details['rsa'][$key]) . "\n";
+				echo "    " . strtolower(Base32::encode(sha1($key_details['rsa'][$key], true))) . "\n";
+			}
+		}
+		break;
+	case OPENSSL_KEYTYPE_DSA:
+		echo "OPENSSL_KEYTYPE_DSA\n";
+		break;
+	case OPENSSL_KEYTYPE_DH:
+		echo "OPENSSL_KEYTYPE_DH\n";
+		break;
+	case OPENSSL_KEYTYPE_EC:
+		echo "OPENSSL_KEYTYPE_EC\n";
+		break;
+	default:
+		echo "unknown\n";
+		break;
 }
